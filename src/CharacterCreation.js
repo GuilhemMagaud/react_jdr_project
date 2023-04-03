88838c50e471f58bbb911d429f5b0daa4ab02e15
hugolanzafameynov <hugo.lanzafame@ynov.com>
2023-04-03 14:23:21 +0200

    Amelioration de la page de création de personnage

--- src/CharacterCreation.js
+++ src/CharacterCreation.js
@@ -1,64 +1,155 @@
-import React, { useState } from 'react';
-import './CharacterCreation.css';
+import React, {useState} from 'react';
+import styled from 'styled-components';
+
+import backgroundImage from './background.png';
+import scrollImage from './scroll.png';
+import {Fonts} from './fonts/CustomFonts';
+
+const CharacterCreationBackground = styled.div
+    `
+      background-image: url(${backgroundImage});
+      background-size: cover;
+      background-position: center;
+      height: 100vh;
+      width: 100vw;
+      position: fixed;
+      top: 0;
+      left: 0;
+      z-index: -1;
+    `;
+
+const ScrollFormContainer = styled.div
+    `
+      background-image: url(${scrollImage});
+      background-size: cover;
+      background-position: center;
+      position: fixed;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      aspect-ratio: 4/5;
+      height: 100%;
+      max-width: 100%;
+    `;
+
+const ScrollFormContainer2 = styled.div
+    `
+      padding: 200px;
+    `;
+
+const ScrollTitle = styled.h1
+    `
+      ${Fonts};
+      font-family: 'Breathe Fire', "MS Gothic", serif;
+      text-align: center;
+      margin-bottom: 50px;
+    `;
+
+const ScrollFormIndividualContainer = styled.div
+    `
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      margin-bottom: 10px;
+    `;
+
+const ScrollLabel = styled.label
+    `
+      ${Fonts};
+      font-family: 'Breathe Fire', "MS Gothic", serif;
+      margin-right: 10px;
+      width: 150px;
+    `;
+
+const ScrollInput = styled.input
+    `
+      flex: 1;
+      background-color: wheat;
+      padding: 10px;
+      border-radius: 5px;
+      margin-right: 10px;
+      width: 100%;
+      max-width: 400px;
+    `;
+
+const ScrollButtonContainer = styled.div
+    `
+        text-align: center;
+    `;
+
+const ScrollButton = styled.button
+    `
+      ${Fonts};
+      font-family: 'Breathe Fire', "MS Gothic", serif;
+      font-size: large;
+      background-color: wheat;
+      border-radius: 5px;
+      margin-top: 25px;
+      padding: 5px;
+    `;
+
+function ScrollForm({htmlFor, label, type, id, name, placeholder, value}) {
+    return (
+        <ScrollFormIndividualContainer>
+            <ScrollLabel htmlFor={htmlFor}>{label}:</ScrollLabel>
+            <ScrollInput type={type} id={id} name={name} placeholder={placeholder} value={value}/>
+        </ScrollFormIndividualContainer>
+    );
+}
+
+function ScrollValidation({label}) {
+    return (
+        <ScrollButtonContainer>
+            <ScrollButton>{label}</ScrollButton>
+        </ScrollButtonContainer>
+    );
+}
 
 function CharacterCreation() {
-  const [name, setName] = useState('');
-  const [classType, setClassType] = useState('');
-  const [race, setRace] = useState('');
-  const [healthPoints, setHealthPoints] = useState(0);
-  const [strength, setStrength] = useState(0);
-  const [intelligence, setIntelligence] = useState(0);
-  const [observation, setObservation] = useState(0);
-  const [agility, setAgility] = useState(0);
-  const [skills, setSkills] = useState('');
+    const [formData, setFormData] = useState({
+        name: '',
+        class: '',
+        race: '',
+        hp: null,
+        strength: null,
+        intelligence: null,
+        observation: null,
+        agility: null,
+        skills: null,
+    });
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-  };
+    const handleSubmit = (event) => {
+        event.preventDefault();
+    };
 
-  return (
-    <div class="global-background">
-    <form class="character-creation-form" onSubmit={handleSubmit}>
-        <label>
-            Name:
-            <input type="text" value={name} onChange={(event) => setName(event.target.value)} />
-        </label>
-        <label>
-            Class:
-            <input type="text" value={classType} onChange={(event) => setClassType(event.target.value)} />
-        </label>
-        <label>
-            Race:
-            <input type="text" value={race} onChange={(event) => setRace(event.target.value)} />
-        </label>
-        <label>
-            Health Points:
-            <input type="number" value={healthPoints} onChange={(event) => setHealthPoints(event.target.value)} />
-        </label>
-        <label>
-            Strength:
-            <input type="number" value={strength} onChange={(event) => setStrength(event.target.value)} />
-        </label>
-        <label>
-            Intelligence:
-            <input type="number" value={intelligence} onChange={(event) => setIntelligence(event.target.value)} />
-        </label>
-        <label>
-            Observation:
-            <input type="number" value={observation} onChange={(event) => setObservation(event.target.value)} />
-        </label>
-        <label>
-            Agility:
-            <input type="number" value={agility} onChange={(event) => setAgility(event.target.value)} />
-        </label>
-        <label>
-            Skills:
-            <input type="text" value={skills} onChange={(event) => setSkills(event.target.value)} />
-        </label>
-        <button type="submit">Create Character</button>
-        </form>
-    </div>
-  );
+    return (
+        <CharacterCreationBackground>
+            <ScrollFormContainer>
+                <ScrollFormContainer2>
+                    <ScrollTitle>Creation du personnage</ScrollTitle>
+                    <ScrollForm htmlFor="name" label="Nom" type="text" id="name"
+                                name="name" value={formData.name}/>
+                    <ScrollForm htmlFor="class" label="Classe" type="text" id="class"
+                                name="class" value={formData.class}/>
+                    <ScrollForm htmlFor="race" label="Race" type="text" id="race"
+                                name="race" value={formData.race}/>
+                    <ScrollForm htmlFor="hp" label="Points de vie" type="number" id="hp"
+                                name="hp" value={formData.hp}/>
+                    <ScrollForm htmlFor="strength" label="Force" type="number" id="strength"
+                                name="strength" value={formData.strength}/>
+                    <ScrollForm htmlFor="intelligence" label="Intelligence" type="number" id="intelligence"
+                                name="intelligence" value={formData.intelligence}/>
+                    <ScrollForm htmlFor="observation" label="Observation" type="number" id="observation"
+                                name="observation" value={formData.observation}/>
+                    <ScrollForm htmlFor="agility" label="Agilité" type="number" id="agility"
+                                name="agility" value={formData.agility}/>
+                    <ScrollForm htmlFor="skills" label="Compétences" type="" id="skills"
+                                name="skills" value={formData.skills}/>
+                    <ScrollValidation label="Creer"/>
+                </ScrollFormContainer2>
+            </ScrollFormContainer>
+        </CharacterCreationBackground>
+    );
 }
 
 export default CharacterCreation;
---
+++ src/fonts/CustomFonts.js
@@ -0,0 +1,11 @@
+import { css } from 'styled-components';
+import BreatheFire from './BreatheFire.otf'
+
+export const Fonts = css`
+  @font-face {
+    font-family: 'Breathe Fire';
+    src: url(${BreatheFire});
+    font-weight: normal;
+    font-style: normal;
+  }
+`;
